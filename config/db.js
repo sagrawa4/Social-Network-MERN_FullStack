0c5660aca5e99c110df5c8d63a4abfdb65103553
Shruti Agrawal <shruti98932@gmail.com>
2021-04-12 17:30:01 -0400

    profile API routes implemented

--- config/db.js
+++ config/db.js
@@ -7,7 +7,8 @@ const connectDB = async () => {
         await mongoose.connect(db, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: false
+            useFindAndModify: false,
+            useCreateIndex: true
         });
         console.log("Mongo db Connected..");
     }catch(err){
